fe497e6efe1a95ea5d4fc7db49e79008cfd67c4d
sxixa <akasyaajibrilian@gmail.com>
2026-09-24 11:12:28 +0700

    test vercel again 2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,9 +10,19 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
+    ->withMiddleware(function (Middleware $middleware) {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->booted(function ($app) {
+        // Explicitly set drivers so Manager::createDriver() never receives null
+        config([
+            'app.maintenance.driver' => 'file',
+            'cache.default' => env('CACHE_STORE', 'array'),
+            'session.driver' => env('SESSION_DRIVER', 'cookie'),
+            'database.default' => env('DB_CONNECTION', 'sqlite'),
+        ]);
+    })
+    ->create();
