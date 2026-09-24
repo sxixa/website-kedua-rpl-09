c90b9f823daf690dcd8af6c16a3eeb6c45ece6bd
sxixa <akasyaajibrilian@gmail.com>
2026-09-24 08:29:25 +0700

    Menambahkan copy dari profil, kontak dan daftar anggota

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/anggota', function () {
+    return view('anggota');
+});
+
+Route::get('/profil', function () {
+    return view('profil');
+});
+
+Route::get('/kontak', function () {
+    return view('kontak');
 });
